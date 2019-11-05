8d6ae6a5fff84850d13329c11076243b07158c34
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-11-04 23:29:03 -0300

    fix(product repository): fix findQualityItems query

--- src/main/java/com/example/sigback/repository/ProductRepository.java
+++ src/main/java/com/example/sigback/repository/ProductRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "select * from order_item where order_item.id in (select product.id from product where product.id in (select product_id from senasa where senasa.review_required = ?1))", nativeQuery = true)
+    @Query(value = "select * from order_item where order_item.product_id in (select product.id from product where product.id in (select product_id from senasa where senasa.review_required = ?1))", nativeQuery = true)
     List<OrderItem> findQualityItems(boolean reviewRequired);
 }
