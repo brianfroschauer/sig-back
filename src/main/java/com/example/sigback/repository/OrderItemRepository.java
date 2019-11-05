a1d4a77a8f08b593071e3d04c69e563c70436286
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-11-05 00:34:36 -0300

    refactor(order item repository): fix query

--- src/main/java/com/example/sigback/repository/OrderItemRepository.java
+++ src/main/java/com/example/sigback/repository/OrderItemRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
  */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
-    @Query(value = "select * from order_item where order_item.order_id in (select purchase_order.id from purchase_order where purchase_order.state = 2)", nativeQuery = true)
+    @Query(value = "select * from order_item where order_item.order_id in (select purchase_order.id from purchase_order where purchase_order.state = 2) and order_item.state = 0", nativeQuery = true)
     List<OrderItem> findValidOrderItems();
 }
