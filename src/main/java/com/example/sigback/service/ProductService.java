98f0425810066d3917d1328c2df7ede2c0f95ea4
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-11-04 23:51:22 -0300

    docs(product service): add javadoc

--- src/main/java/com/example/sigback/service/ProductService.java
+++ src/main/java/com/example/sigback/service/ProductService.java
@@ -29,7 +29,7 @@ public interface ProductService {
     /**
      * Find all quality items.
      *
-     * @return
+     * @return all quality items.
      */
     List<OrderItem> findQualityItems();
 
