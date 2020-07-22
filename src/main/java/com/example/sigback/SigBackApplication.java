dbccb87f9f8240c919d3ced83abc0494d9e7c764
matiasvenditti <mativenditti@gmail.com>
2020-07-22 19:16:19 -0300

    added dockerfile and cors filter

--- src/main/java/com/example/sigback/SigBackApplication.java
+++ src/main/java/com/example/sigback/SigBackApplication.java
@@ -2,12 +2,27 @@ package com.example.sigback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SigBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigBackApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedOrigins("*");
+			}
+		};
 	}
 
 }
